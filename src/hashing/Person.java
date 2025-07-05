d863017c50e7f1b78340be38990b6f00144a8557
Darshan <darshanbk812@gmail.com>
2025-07-05 23:19:24 +0530

    explined concepts

--- src/hashing/Person.java
+++ src/hashing/Person.java
@@ -5,6 +5,16 @@ import java.util.Iterator;
 import java.util.Objects;
 import java.util.Set;
 
+//->Hash table uses a hashing technique  to store the data for backet array
+//->Hasing technique which is used to find hashcode value using hasing funtion  
+//->to find index for object in array uses a modulous funtion ex:- index = hashcode % array.length
+//->after getting a index the key of the set  ot map data will be store in the particular index of the array by creating new node 
+//-> Before storing data in the form of node  1st it will check in the  index weather is already have node why it happens means
+//->Some times objects may get same hashcode with a same index in that time data will stores in the same index by appending the new node with a exsited node
+//->Before appending the new node the rule is to not store the duplicate keys in table so if we soring a 2 or 3 nodes with a same hashCode and index for the object 
+//->to ensure that we use equals method to check the key is already present with a same name in the memory if it is same it just replace it or else append the node with a key ,value, adress of the next node and previous node
+//->This is called hash collison
+
 public class Person {
 
 	String name;
